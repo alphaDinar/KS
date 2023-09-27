e9b2c8f22a2c998147b5c863062cc53ef5156d10
cathsuchurch <cathsuchurch@gmail.com>
2023-09-27 15:02:21 +0000

    change

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const topNav = document.querySelector('.topNav')
+topNav.innerHTML = `
+<logo-tag>
+<img src="logo2.png" alt="">
+</logo-tag>
+<nav>
+<a href="index.html">Home</a>
+<a href="">About</a>
+<a href="">HelloKS</a>
+<a href="">Contact</a>
+<a href="donate.html" id="donateSmall">Donate</a>
+</nav>
+<button class="material-symbols-outlined" onclick="toggleMenu()">menu</button>
+`
+
+const topNavMenu = document.querySelector('.topNav nav')
+
+const toggleMenu =()=>{
+  topNavMenu.classList.toggle('change')
+}
