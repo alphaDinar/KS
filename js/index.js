e2b5e4f765b32f7f81edf7ffe3cb604d2bcbeacc
cathsuchurch <cathsuchurch@gmail.com>
2023-10-02 12:31:53 +0000

    change

--- js/index.js
+++ js/index.js
@@ -1,22 +1,51 @@
 const topNav = document.querySelector(".topNav");
-topNav.innerHTML = `
-<logo-tag>
-<img src="logo2.png" alt="">
-</logo-tag>
-<nav>
-<a href="index.html">Home</a>
-<a href="about.html">About</a>
-<a href="blog.html">HelloKS</a>
-<a href="contact.html">Contact</a>
-<a href="donate.html" id="donateSmall">Donate</a>
-</nav>
-<button class="material-symbols-outlined" onclick="toggleMenu()">menu</button>
-`;
+if (topNav) {
+  topNav.innerHTML = `
+    <logo-tag>
+    <img src="Logos/logo2.png" alt="">
+    </logo-tag>
+    <nav>
+    <a href="index.html">Home</a>
+    <a href="about.html">About</a>
+    <a href="blog.html">HelloKS</a>
+    <a href="contact.html">Contact</a>
+    <a href="donate.html" id="donateSmall">Donate</a>
+    </nav>
+    <button class="material-symbols-outlined" onclick="toggleMenu()">menu</button>
+    `;
+}
 
 const topNavMenu = document.querySelector(".topNav nav");
-
-topNavMenu.addClassName = "menu";
 
 const toggleMenu = () => {
   topNavMenu.classList.toggle("change");
 };
+
+
+const getTimeSince = (unixtime) => {
+  const time = new Date(unixtime)
+  const now = new Date();
+  const timeDiff = now - time; // Calculate the time difference in milliseconds
+
+  const minutes = Math.floor(timeDiff / (1000 * 60)); // Calculate minutes
+  const hours = Math.floor(timeDiff / (1000 * 60 * 60)); // Calculate hours
+  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24)); // Calculate days
+
+  if (minutes < 60) {
+    return `${minutes} min${minutes !== 1 ? 's' : ''} ago`;
+  } else if (hours < 24) {
+    return `${hours} hr${hours !== 1 ? 's' : ''} ago`;
+  } else {
+    return `${days} day${days !== 1 ? 's' : ''} ago`;
+  }
+}
+
+const loader = document.querySelector('.loader')
+
+const startLoader = () => {
+  loader.classList.remove('hide')
+}
+const endLoader = () => {
+  loader.classList.add('hide')
+}
+
